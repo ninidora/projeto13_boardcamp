83109f96b6adddacc87ac8b2d075fd6704147663
Tarcísio Teles <tfst@ecomp.poli.br>
2021-10-14 22:34:51 -0300

    test: started validation with 'joi'

--- api-boardcamp/boardcampServer.js
+++ api-boardcamp/boardcampServer.js
@@ -1,22 +1,58 @@
 import express from 'express';
 import pg from 'pg';
+import joi from 'joi';
+
+const roleSettings = {
+  user: 'bootcamp_role',
+  password: 'senha_super_hiper_ultra_secreta_do_role_do_bootcamp',
+  host: 'localhost',
+  port: 5432,
+  database: 'boardcamp'
+};
+
+const queriesSchema = joi.object({
+  categoryName: joi.string().alphanum().required(),
+})
 
 const { Pool } = pg;
-const connection = new Pool({
-    user: 'bootcamp_role',
-    password: 'senha_super_hiper_ultra_secreta_do_role_do_bootcamp',
-    host: 'localhost',
-    port: 5432,
-    database: 'boardcamp'
-  });
+const connection = new Pool(roleSettings);
+const app = express();
+app.use(express.json());
 
-  const app = express();
-  app.use(express.json());
 
-  app.get('/categories', (req, res) => {
-    const query = connection.query('SELECT * FROM categories;');
-    query.then(result => { 
-      res.status(200).send(result.rows); });
-  });
+app.get('/categories', async (req, res) => {
+  try {
+  const categoriesPromise = await connection.query('SELECT * FROM categories;');
+  console.log(categoriesPromise);
+  res.status(200).send(categoriesPromise.rows);
+  } catch(error) {
+    console.log('ERRO!:', error);
+  }
+});
 
-  app.listen(4000, () => {console.log('Server listening, :4000.')});
+/*
+app.post('/categories', async (req, res) => {
+  
+  
+
+});
+*/
+const testJoi = {categoryName: '' };
+queriesSchema.validate(testJoi).error ? console.log('erro') : console.log('passed');
+console.log('joi:', queriesSchema.validate(testJoi).error);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(4000, () => { console.log('Server listening, :4000.') });
